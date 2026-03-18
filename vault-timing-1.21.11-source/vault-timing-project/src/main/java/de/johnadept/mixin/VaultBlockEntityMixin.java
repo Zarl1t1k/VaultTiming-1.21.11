1b2e27739a89c05f921c0aba85894b0d72d1e965
Zarl1t1k <zarl1t1k@gmail.com>
2026-03-18 13:03:43 +0200

    Update VaultBlockEntityMixin.java

--- vault-timing-1.21.11-source/vault-timing-project/src/main/java/de/johnadept/mixin/VaultBlockEntityMixin.java
+++ vault-timing-1.21.11-source/vault-timing-project/src/main/java/de/johnadept/mixin/VaultBlockEntityMixin.java
@@ -1,6 +1,5 @@
 package de.johnadept.mixin;
 
-import de.johnadept.VaultTiming;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.VaultBlockEntity;
 import net.minecraft.block.vault.VaultConfig;
@@ -22,61 +21,27 @@ import java.util.List;
 @Mixin(VaultBlockEntity.Server.class)
 public class VaultBlockEntityMixin {
 
-    @Inject(
-            method = "tryUnlock",
-            at = @At("HEAD"),
-            cancellable = true
-    )
+    @Inject(method = "tryUnlock", at = @At("HEAD"), cancellable = true)
     private static void onTryUnlock(
-            ServerWorld world,
-            BlockPos pos,
-            BlockState state,
-            VaultConfig config,
-            VaultServerData serverData,
-            VaultSharedData sharedData,
-            PlayerEntity player,
-            ItemStack stack,
-            Hand hand,
-            CallbackInfo ci
-    ) {
-        System.out.println("Executing tryUnlock");
-
+            ServerWorld world, BlockPos pos, BlockState state,
+            VaultConfig config, VaultServerData serverData, VaultSharedData sharedData,
+            PlayerEntity player, ItemStack stack, Hand hand, CallbackInfo ci) {
         try {
             if (!isValidKey(config, stack)) {
                 VaultBlockEntity.Server.playFailedUnlockSound(world, serverData, pos,
                         net.minecraft.sound.SoundEvents.BLOCK_VAULT_REJECT_REWARDED_PLAYER);
                 return;
             }
-
             if (serverData.hasRewardedPlayer(player)) {
                 VaultBlockEntity.Server.playFailedUnlockSound(world, serverData, pos,
                         net.minecraft.sound.SoundEvents.BLOCK_VAULT_REJECT_REWARDED_PLAYER);
                 return;
             }
-
-            boolean isOminous = state.get(net.minecraft.block.VaultBlock.OMINOUS);
+            ci.cancel();
             List<ItemStack> lootItems = new ArrayList<>();
-
-            if (isOminous) {
-                ci.cancel();
-                lootItems.add(sharedData.getDisplayItem().copy());
-                if (world.getGameRules().getBoolean(VaultTiming.SHOULD_ALSO_DROP_DEFAULT_LOOT)) {
-                    lootItems.addAll(VaultBlockEntity.Server.generateLoot(world, config, pos, player, stack));
-                }
-            } else {
-                if (!world.getGameRules().getBoolean(VaultTiming.SHOULD_MODIFY_NON_OMINOUS_VAULTS)) {
-                    return;
-                }
-                ci.cancel();
-                lootItems.add(sharedData.getDisplayItem().copy());
-                if (world.getGameRules().getBoolean(VaultTiming.SHOULD_ALSO_DROP_DEFAULT_LOOT)) {
-                    lootItems.addAll(VaultBlockEntity.Server.generateLoot(world, config, pos, player, stack));
-                }
-            }
-
+            lootItems.add(sharedData.getDisplayItem().copy());
             stack.decrement(config.keyItem().getCount());
             VaultBlockEntity.Server.unlock(world, state, pos, config, serverData, sharedData, lootItems);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
