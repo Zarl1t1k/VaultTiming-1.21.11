7399b6d977c452e91ffac3753c7faa611c0e159a
Zarl1t1k <zarl1t1k@gmail.com>
2026-03-18 13:13:08 +0200

    Update VaultTiming.java

--- vault-timing-1.21.11-source/vault-timing-project/src/main/java/de/johnadept/VaultTiming.java
+++ vault-timing-1.21.11-source/vault-timing-project/src/main/java/de/johnadept/VaultTiming.java
@@ -1,6 +1,7 @@
 package de.johnadept;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,8 +9,23 @@ public class VaultTiming implements ModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("vault-timing");
 
+    public static GameRules.Key<GameRules.BooleanRule> SHOULD_MODIFY_NON_OMINOUS_VAULTS;
+    public static GameRules.Key<GameRules.BooleanRule> SHOULD_ALSO_DROP_DEFAULT_LOOT;
+
     @Override
     public void onInitialize() {
         LOGGER.info("Initializing Vault Timing");
+
+        SHOULD_MODIFY_NON_OMINOUS_VAULTS = GameRules.register(
+                "modifyNonOminousVaults",
+                GameRules.Category.MISC,
+                GameRules.BooleanRule.create(true)
+        );
+
+        SHOULD_ALSO_DROP_DEFAULT_LOOT = GameRules.register(
+                "vaultsShouldAlsoDropDefaultLoot",
+                GameRules.Category.MISC,
+                GameRules.BooleanRule.create(false)
+        );
     }
 }
